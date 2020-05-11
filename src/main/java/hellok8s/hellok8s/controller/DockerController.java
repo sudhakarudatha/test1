4c0aba6a381d8a13edf602293770e1efd97eadb0
sudhakarudatha <sudhakar.kavur@gmail.com>
2020-05-11 18:15:29 +0530

    Update DockerController.java

--- src/main/java/hellok8s/hellok8s/controller/DockerController.java
+++ src/main/java/hellok8s/hellok8s/controller/DockerController.java
@@ -2,6 +2,7 @@ package hellok8s.hellok8s.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.seec.insurance;
 
 @RestController
 public class DockerController {
